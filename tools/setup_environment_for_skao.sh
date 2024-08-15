e1fd0ed415f5c0e2573bc8078ef30bb6585ce98f
rob barnsley <rob.barnsley@skao.int>
2024-08-15 13:25:50 +0100

    adding tools/setup_environment_for_skao.sh script for easier guided usage

--- tools/setup_environment_for_skao.sh
+++ tools/setup_environment_for_skao.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup envvars
-RUCIO_TASK_MANAGER_ROOT=`python3 -c "import os,sys; print(os.path.realpath(sys.argv[1]))" ../../`
+RUCIO_TASK_MANAGER_ROOT=`python3 -c "import os,sys; print(os.path.realpath(sys.argv[1]))" ../`
 read -e -p "Path to Rucio task manager root (default: $RUCIO_TASK_MANAGER_ROOT): " input
 RUCIO_TASK_MANAGER_ROOT=${input:-$RUCIO_TASK_MANAGER_ROOT} 
 export RUCIO_TASK_MANAGER_ROOT=$RUCIO_TASK_MANAGER_ROOT
@@ -17,7 +17,7 @@ export RUCIO_CFG_ACCOUNT=$RUCIO_CFG_ACCOUNT
 echo "RUCIO_CFG_ACCOUNT set to \"$RUCIO_CFG_ACCOUNT\""
 
 # Get a token
-docker run -itd --name ska-rucio-client --rm -e PYTHONWARNINGS="ignore:Unverified HTTPS request" -e RUCIO_CFG_ACCOUNT=$RUCIO_CFG_ACCOUNT -e RUCIO_CFG_AUTH_TYPE=oidc registry.gitlab.com/ska-telescope/src/ska-rucio-client:release-1.29.0
+docker run -itd --name ska-rucio-client --rm -e PYTHONWARNINGS="ignore:Unverified HTTPS request" -e RUCIO_CFG_ACCOUNT=$RUCIO_CFG_ACCOUNT -e RUCIO_CFG_AUTH_TYPE=oidc registry.gitlab.com/ska-telescope/src/src-dm/ska-src-dm-da-rucio-client:release-1.29.6
 
 docker exec -it ska-rucio-client rucio whoami && export BEARER_TOKEN=`docker exec -it ska-rucio-client cat /tmp/user/.rucio_user/auth_token_for_account_$RUCIO_CFG_ACCOUNT`
 docker stop ska-rucio-client
@@ -25,7 +25,7 @@ docker stop ska-rucio-client
 export OIDC_ACCESS_TOKEN=$BEARER_TOKEN
 
 # Build the task manager image
-(cd $RUCIO_TASK_MANAGER_ROOT; make skao)
+(cd $RUCIO_TASK_MANAGER_ROOT; make build-skao)
 
 # Define a quick entrypoint
 function run-task () {
@@ -45,3 +45,6 @@ function run-task () {
   --name=rucio-task-manager rucio-task-manager:`cat $RUCIO_TASK_MANAGER_ROOT/BASE_RUCIO_CLIENT_TAG`
 }
 
+echo
+echo "To run task, run run-task <task-name>."
+echo
