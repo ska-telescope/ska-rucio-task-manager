f6b7da0da28bac092e9cdff2b370b0ef96237af3
James Collinson <james.collinson@skao.int>
2024-10-23 09:05:46 +0100

    MAG-977 Remove unnecessary kwargs and nested RSE loop

--- src/tasks/tests/replicate_dataset.py
+++ src/tasks/tests/replicate_dataset.py
@@ -2,14 +2,12 @@ import copy
 import os
 import random
 
-from rucio.client.uploadclient import Client
 from rucio.client.didclient import DIDClient
+from rucio.client.ruleclient import RuleClient
 
-from elasticsearch import Elasticsearch
-
-from common.rucio.helpers import createCollection
 from tasks.task import Task
 from utility import bcolors
+
 
 class TestReplication(Task):
     """ Rucio dataset replication to a list of RSEs. """
@@ -17,26 +15,22 @@ class TestReplication(Task):
     def __init__(self, logger):
         super().__init__(logger)
         self.activity = None
+        self.scope = None
         self.datasetName = None
         self.rses = None
-        self.scope = None
         self.lifetime = None
         self.outputDatabases = None
-        self.taskName = None
-        self.namingPrefix = None
 
     def run(self, args, kwargs):
         super().run()
         self.tic()
         try:
             self.activity = kwargs["activity"]
+            self.scope = kwargs["scope"]
             self.datasetName = kwargs["datasetName"]
             self.rses = kwargs["rses"]
-            self.scope = kwargs["scope"]
             self.lifetime = kwargs["lifetime"]
             self.outputDatabases = kwargs["output"]["databases"]
-            self.taskName = kwargs["task_name"]
-            self.namingPrefix = kwargs.get("naming_prefix", "")
         except KeyError as e:
             self.logger.critical("Could not find necessary kwarg for task.")
             self.logger.critical(repr(e))
@@ -59,55 +53,48 @@ class TestReplication(Task):
                 filters={'name': name_pattern},
                 did_type='dataset'
             ))
-            print(datasets)
+            self.logger.debug(
+                bcolors.OKBLUE +
+                "Found datasets {}".format(datasets) +
+                bcolors.ENDC
+            )
             if not datasets:
                 self.logger.error(
                     bcolors.FAIL +
-                    "Failed to find datasets" +
+                    "No datasets found matching {}".format(name_pattern) +
                     bcolors.ENDC
                 )
                 return False
         except Exception as e:
             self.logger.error(
                 bcolors.FAIL +
-                "Failed to find latest dataset: {}".format(str(e)) +
+                "Error when listing datasets: {}".format(str(e)) +
                 bcolors.ENDC
             )
             return False
 
         # Add replication rules for the dataset
-        for rseSrc in self.rses:
-            rse_n_success = 0
-            rse_n_fail = 0
-            self.logger.info(
-                bcolors.OKBLUE + "RSE (src): {}".format(rseSrc) + bcolors.ENDC
-            )
-
-            self.logger.debug("Adding replication rules...")
-            for rseDst in self.rses:
-                if rseSrc == rseDst:
-                    continue
-                self.logger.debug(
-                    bcolors.OKGREEN
-                    + "RSE (dst): {}".format(rseDst)
-                    + bcolors.ENDC
+        for rse in self.rses:
+            self.logger.info("Adding replication rule for RSE {}".format(rse))
+            try:
+                client = RuleClient(logger=self.logger)
+                rtn = client.add_replication_rule(
+                    dids=[{"scope": self.scope, "name": self.datasetName}],
+                    copies=1,
+                    rse_expression=rse,
+                    lifetime=self.lifetime,
+                    activity=self.activity,
+                    asynchronous=False,
                 )
-                try:
-                    client = Client(logger=self.logger)
-                    rtn = client.add_replication_rule(
-                        dids=[{"scope": self.scope, "name": self.datasetName}],
-                        copies=1,
-                        rse_expression=rseDst,
-                        lifetime=self.lifetime,
-                        activity=self.activity,
-                        source_replica_expression=rseSrc,
-                        asynchronous=False,
-                    )
-                    self.logger.debug("Rule ID: {}".format(rtn[0]))
-                except Exception as e:
-                    self.logger.warning(repr(e))
-                    continue
-                self.logger.debug("Replication rules added")
+                self.logger.debug("Rule ID: {}".format(rtn[0]))
+            except Exception as e:
+                self.logger.warning("Unable to add replication rule: {}".format(repr(e)))
+                return False
+            self.logger.info(
+                bcolors.OKGREEN +
+                "Replication rules added" +
+                bcolors.ENDC
+            )
 
         # Push task output to databases.
         if self.outputDatabases is not None:
